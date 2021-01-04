48f86f30a101553ea697378e7301c06145b4af0d
Davidmurf1 <davidmurphy-bfort@hotmail.com>
2021-01-04 17:36:04 +0000

    new game

--- script.js
+++ script.js
@@ -1,96 +1,196 @@
 
 
-const numbers =['Ace','2', '3', '4', '5', '6', '7', '8','9','10','Jack','Queen','King'];
-const suits = [' of Hearts', ' of Clubs', ' of Diamonds', ' of Spades' ];
-deck = [];
-
-function gather () {
-for (let i = 0; i < numbers.length; i++) {
-for (let j=0 ; j < suits.length; j++){
-
- deck.push(numbers[i] + suits[j])  }}
-}
-gather()
-
-console.log(deck)
-
-
-
-
-
-
-
- /* Fisher-Yates Shuffle
- Note::
- X begins at index position 1, then loops in reverse through deck
- Y is a random number, the original lenght of X
- generate number between 0-52,then 52 etc
- assigns temp the value of x
- then assigns x the position of y
- the temp the position of y;
-
-
-	function shuffle (deck) {
- 	 var x = 0
-    , y = 0
-    , temp = null
-
-     
-
-  for (x = array.length - 1; x > 0; x -= 1) {
-    y = Math.floor(Math.random() * (x + 1))
-    temp = deck[x]
-    deck[x] = deck[y]
-    deck[y] = temp
-  }
+class card {
+    constructor() {
+        this.card = card;
+        this.img = img;
+    }
 }
 
-document.write(array);
+class Deck {
+    constructor(){
+        this.cards = [];
+    }
+        assemble() {
+        let colours = ['Red', 'Green', 'Purple', 'Yellow'];
+        let img = [];
+       for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 4; i++){
+            this.cards.push(colours[i])}
+        }
+       }
+       shuffle(){
+        let i = this.cards.length, j, temp;
+        while ( --i > 0) 
+        {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = this.cards[j];
+        this.cards[j] = this.cards[i];
+        [j] = this.cards[i];
+        this.cards[i] = temp;
+        }
+        return this.cards;
+}
+       }
+   
+    class Dealer {
+    constructor() {
+    this.sixPlayingCards = [];
+    }
+    deal(){
+    let play = new Deck;
+    play.assemble();
+    play.shuffle();
+    this.sixPlayingCards = play.cards.slice(0,6);
+    
+}
+
+}
+
+class Player {
+    constructor(){
+        this.player1Card1 = [];
+        this.player1Card2 = [];
+        this.player2Card1 = [];
+        this.player2Card2 = [];
+        this.river1 = [];
+         this.river2 = [];
+    }
+    assign(cards) {
+                this.player1Card1 = cards.slice(0,1);
+                this.player1Card2 = cards.slice(1,2);
+                this.player2Card1 = cards.slice(2,3);
+                this.player2Card2 = cards.slice(3,4);
+                this.river1 =        cards.slice(4,5);
+                this.river2 =        cards.slice(5,6);
+    }
+}
+
+class Game {
+    constructor() {
+    
+    this.players = [];
+    
+
+    }
+    play() {
+        
+      let go = new Dealer;
+      go.deal();
+      let poker = new Player;
+      poker.assign(go.sixPlayingCards);
+      console.log([poker.player1Card1, poker.player1Card2, poker.player2Card1, poker.player2Card2, poker.river1, poker.river2])
+
+      var a = poker.player1Card1.toString();
+      var b = poker.player1Card2.toString();
+      var c = poker.player2Card1.toString();
+      var d = poker.player2Card2.toString();
+      var e =       poker.river1.toString();
+      var f =       poker.river2.toString();
+
+ /* 
+ if (c === 'Red' ) {document.getElementById("cardA").src = "img/Red_back.jpg"}
+
+    else if (c === 'Green' ) {document.getElementById("cardA").src = "img/Green_back.jpg"}
+
+    else if (c === 'Purple' ) {document.getElementById("cardA").src = "img/purple_back.jpg"}
+
+    else {document.getElementById("cardA").src = "img/Yellow_back.jpg"}
+
+
+    if (d === 'Red' ) {document.getElementById("cardB").src = "img/Red_back.jpg"}
+
+    else if (d === 'Green' ) {document.getElementById("cardB").src = "img/Green_back.jpg"}
+
+    else if (d === 'Purple' ) {document.getElementById("cardB").src = "img/purple_back.jpg"}
+
+    else {document.getElementById("cardB").src = "img/Yellow_back.jpg"}
+
+
+
+
+   */
+
+
+
+
+    if (a === 'Red' ) {document.getElementById("cardC").src = "img/Red_back.jpg"}
+
+    else if (a === 'Green' ) {document.getElementById("cardC").src = "img/Green_back.jpg"}
+
+    else if (a === 'Purple' ) {document.getElementById("cardC").src = "img/purple_back.jpg"}
+
+    else {document.getElementById("cardC").src = "img/Yellow_back.jpg"}
+
+
+    if (b === 'Red' ) {document.getElementById("cardD").src = "img/Red_back.jpg"}
+
+    else if (b === 'Green' ) {document.getElementById("cardD").src = "img/Green_back.jpg"}
+
+    else if (b === 'Purple' ) {document.getElementById("cardD").src = "img/purple_back.jpg"}
+
+    else {document.getElementById("cardD").src = "img/Yellow_back.jpg"}
+
+
+    if (e === 'Red' ) {document.getElementById("cardE").src = "img/Red_back.jpg"}
+
+    else if (e === 'Green' ) {document.getElementById("cardE").src = "img/Green_back.jpg"}
+
+    else if (e === 'Purple' ) {document.getElementById("cardE").src = "img/purple_back.jpg"}
+
+    else {document.getElementById("cardE").src = "img/Yellow_back.jpg"}
+
+
+    if (f === 'Red' ) {document.getElementById("cardF").src = "img/Red_back.jpg"}
+
+    else if (f === 'Green' ) {document.getElementById("cardF").src = "img/Green_back.jpg"}
+
+    else if (f === 'Purple' ) {document.getElementById("cardF").src = "img/purple_back.jpg"}
+
+    else {document.getElementById("cardF").src = "img/Yellow_back.jpg"}
+ 
+}
+};
+
+function deal(){
+let goo = new Game;
+goo.play();
+
+
+    }
+
+
+
+/*
+function play ()
+if 
 
 */
 
-deck.shuffle = function() {
-
-var i = this.length, j, temp;
-while ( --i > 0) 
-{
-j = Math.floor(Math.random() * (i + 1));
-temp = this[j];
-this[j] = this[i];
-this[i] = temp;
-}
-return this;
-}
-
-var result = deck.shuffle();
-console.log(result)
 
 
-/*Include 0 in selection as it is a card */
-let empty = [];
 
-for (i=0; i < 7; i++) {
-function randomSelect(minimum, maximum) {
-return Math.floor(Math.random() * (maximum - minimum)) + minimum;
-}
-let choice = randomSelect(0,53)
-let pick = empty.push(choice);
-}
-console.log(empty)
+ 
 
 
-let cardA = result[empty[0]];
-let cardB = result[empty[1]];
-let cardC = result[empty[2]];
-let cardD = result[empty[3]];
-let cardE = result[empty[4]];
-let cardF = result[empty[5]];
-let cardG = result[empty[6]];
 
-console.log(cardA);
-console.log(cardB);
-console.log(cardC);
-console.log(cardD);
-console.log(cardE);
-console.log(cardF);
-console.log(cardG);
+ 
+/*
+ ***Card Rankings***
+-- High card - player with highest value card in the event of no other combination
+-- Pair - Any combination of two of the same value cards
+-- Two pair - Any combination of two of the same value cards, held twice
+-- Three of a kind - Any combination of three of the same value cards
+-- Straight - Any combination of five cards in cronological incrementing order
+-- Flush - Five cards of equal suit
+-- Full house - A combination of three and two pairs of any combination
+-- Four of a kind - Any combination of four of the same value cards
+-- Straight Flush - A combination of five cards of the same suit in cronological order
+-- Royal Flush - Highest combination, the Broadway of Diamonds (K,Q,J,10,A)
+
+***LOGIC***
+will use if statements 
+
+if (cardA = 2 && cardB || cardC || cardD ||cardE || cardF || cardG ||  = 2 ) {return 'pair'}
+
+  */
